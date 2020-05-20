e989982bcf97d4026801353bfa1c509a672bc19f
sam <sblan45@gmail.com>
2020-05-20 13:45:49 -0400

    add both exercises

---
+++ employee.rb
@@ -0,0 +1,18 @@
+# 4 cores of info first last salary active status (true or false)
+
+# store this data using diff data structures
+
+# Employee1 = ["john", "Blank", 60000, true]
+# Employee2 = ["sam", "Carter", 25000, true]
+
+# # puts Employee1[0] + " " + Employee1[1] + " makes " + Employee1[2].to_s + " dollars a year."
+# puts "#{Employee1[0]} #{Employee1[1]} makes #{Employee1[2]} dollars a year"
+
+
+Employee1 = {:first_name => "John", :last_name => "Blank", :salary => 60000, :status => true}
+Employee2 = {:first_name => "sam", :last_name => "familna", :salary => 45000, :status => true}
+
+puts "#{Employee1[:first_name]} #{Employee1[:last_name]} makes #{Employee1[:salary]} a year"
+
+puts Employee1
+
---
+++ store_item.rb
@@ -0,0 +1,10 @@
+car1 = {color: "red", price: 65000, year: 2020}
+car2 = {color: "blue", price: 12000, year: 2009}
+car3 = {color: "purple", price: 23500, year: 2018}
+car4 = {"color" => "blue", "price" => 13000, "year" => 2017}
+car5 = {:color => "silver", :price => 15000, :year => 2012} 
+
+puts "car 1 is a #{car1[:year]} #{car1[:color]} car at a price of #{car1[:price]} "
+
+p car5
+p car4
